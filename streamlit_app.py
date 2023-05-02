a46e5dc20ef9c10e0f3e4ad0565c2a57e894d5fa
snowtrialjd <131877359+snowtrialjd@users.noreply.github.com>
2023-05-02 16:59:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 
-streamlit.header("The fruit list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
